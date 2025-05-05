5c1469c9a5a320af1a576ad1c91d73db2c008de6
Evgenii <vansik8@gmail.com>
2025-05-05 02:15:16 +0000

    change leadpage

---
+++ webmain/__init__.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+from flask import Flask, render_template, url_for, request, Response
+from http import HTTPStatus
+from webmain.database import migrate, Session
+from webmain.routes import blueprint
+# from webmain.model import Products, Category
+
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+def internal_server_error(e):
+  return render_template('500.html'), 500
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    app.register_blueprint(blueprint)
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
+    migrate()
+
+    return app
+
+#Создаем экземпляр для Gunicorn
+app = create_app()
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
--- webmain/populaite.py
+++ webmain/populaite.py
@@ -3,85 +3,91 @@ from webmain.database import Session, Base
 from webmain.model import Products, Category
 
 list_products = [
-    # {
-    #     "name": "Торт сливочный",
-    #     "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
-    #     "price": 330,
-    #     "category": "torts",
-    #     "link_photo": "images/torts.jpg",
-    #     "link_like": "images/like.svg",
-    #     "link_basket": "images/basket.svg"
-    # },
-    # {
-    #     "name": "Торт шоколадный",
-    #     "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
-    #     "price": 400,
-    #     "category": "torts",
-    #     "link_photo": "images/torts.jpg",
-    #     "link_like": "images/like.svg",
-    #     "link_basket": "images/basket.svg"
-    # },
-    # {
-    #     "name": "Маффин шоколадный",
-    #     "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
-    #     "price": 330,
-    #     "category": "maffiny",
-    #     "link_photo": "images/maffiny.jpg",
-    #     "link_like": "images/like.svg",
-    #     "link_basket": "images/basket.svg"
-    # },
-    # {
-    #     "name": "Пирог вишневый",
-    #     "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
-    #     "price": 330,
-    #     "category": "pirogi",
-    #     "link_photo": "images/pirogi.jpg",
-    #     "link_like": "images/like.svg",
-    #     "link_basket": "images/basket.svg"
-    # },
-    # {
-    #     "name": "Меренге",
-    #     "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
-    #     "price": 330,
-    #     "category": "merenge",
-    #     "link_photo": "images/merenge.jpg",
-    #     "link_like": "images/like.svg",
-    #     "link_basket": "images/basket.svg"
-    # }
+    {
+        "name": "Торт сливочный",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "price": 330,
+        "category": "torts",
+        "link_photo": "images/torts.jpg",
+        "link_like": "images/like.svg",
+        "link_basket": "images/basket.svg"
+    },
+    {
+        "name": "Торт шоколадный",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "price": 400,
+        "category": "torts",
+        "link_photo": "images/torts.jpg",
+        "link_like": "images/like.svg",
+        "link_basket": "images/basket.svg"
+    },
+    {
+        "name": "Маффин шоколадный",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "price": 330,
+        "category": "maffiny",
+        "link_photo": "images/maffiny.jpg",
+        "link_like": "images/like.svg",
+        "link_basket": "images/basket.svg"
+    },
+    {
+        "name": "Пирог вишневый",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "price": 330,
+        "category": "pirogi",
+        "link_photo": "images/pirogi.jpg",
+        "link_like": "images/like.svg",
+        "link_basket": "images/basket.svg"
+    },
+    {
+        "name": "Меренге",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "price": 330,
+        "category": "merenge",
+        "link_photo": "images/merenge.jpg",
+        "link_like": "images/like.svg",
+        "link_basket": "images/basket.svg"
+    }
 ]
 
 list_category = [
-    # {
-    #     "name": "Торты",
-    #     "description": "Большая разновидность тортов, на любой праздник и торжество",
-    #     "link_name": "torts",
-    #     "link_photo": "images/torts.jpg"
-    #
-    # },
-    # {
-    #     "name": "Маффины",
-    #     "description": "Маффины это просто вкусный перекус для любого удобного брейка с кофем",
-    #     "link_name": "maffiny",
-    #     "link_photo": "images/maffiny.jpg"
-    # },
-    # {
-    #     "name": "Пироги",
-    #     "description": "Выбор пирогов для любого дня когда времени не хватает а хочется чего нибудь вкусного",
-    #     "link_name": "pirogi",
-    #     "link_photo": "images/pirogi.jpg"
-    # },
-    # {
-    #     "name": "Меренге",
-    #     "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
-    #     "link_name":  "merenge",
-    #     "link_photo": "images/merenge.jpg"
-    # }
+    {
+        "name": "Торты",
+        "description": "Большая разновидность тортов, на любой праздник и торжество",
+        "link_name": "torts",
+        "link_photo": "images/torts.jpg"
+    },
+    {
+        "name": "Маффины",
+        "description": "Маффины это просто вкусный перекус для любого удобного брейка с кофем",
+        "link_name": "maffiny",
+        "link_photo": "images/maffiny.jpg"
+    },
+    {
+        "name": "Пироги",
+        "description": "Выбор пирогов для любого дня когда времени не хватает а хочется чего нибудь вкусного",
+        "link_name": "pirogi",
+        "link_photo": "images/pirogi.jpg"
+    },
+    {
+        "name": "Меренге",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "link_name": "merenge",
+        "link_photo": "images/merenge.jpg"
+    },
+    {
+        "name": "Keksi",
+        "description": "Прийдя в гости не с пустыми руками уже хорошо, красивые, вкусные по доступной цене",
+        "link_name": "Keks",
+        "link_photo": "images/merenge.jpg"
+    }
 ]
-
 
 def populate():
     conn = Session()
     for category in list_category:
+        existing_category = conn.query(Category).filter_by(link_name=category["link_name"]).first()
+        if not existing_category:
         new_category = Category(
             name=category["name"],
             description=category["description"],
@@ -95,6 +101,8 @@ def populate():
         print("----", category_products)
 
         for product in category_products:
+            existing_product = conn.query(Products).filter_by(name=product["name"]).first()
+            if not existing_product:
             new_product = Products(
                 name=product["name"],
                 description=product["description"],
